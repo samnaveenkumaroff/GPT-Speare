e7029f6572795a9ab4eeb02825647a0e3fe8234c
Sam Naveenkumar .V <143938999+samnaveenkumaroff@users.noreply.github.com>
2025-06-18 18:06:16 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ References:
 1) the official GPT-2 TensorFlow implementation released by OpenAI:
 https://github.com/openai/gpt-2/blob/master/src/model.py
 2) huggingface/transformers PyTorch implementation:
-https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
+https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py trial kit
 """
 
 import math
